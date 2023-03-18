15c5cc522e33981bbb470dc00db97f9f72d38441
lijun <1094453726@qq.com>
2023-03-18 17:17:33 +0800

    第一个元素不需要margin left -1px;

--- src/button.vue
+++ src/button.vue
@@ -1,12 +1,38 @@
 <template>
-  <button class="g-button">按钮</button>
+  <button  @click="$emit('click')" class="g-button" :class="{[`icon-${iconPosition}`]:true}">
+    <g-icon class='icon' v-if="icon && !loading" :name="icon"></g-icon>
+    <g-icon v-if="loading" class='loading icon'  name="loading"></g-icon>
+    <div class="content">
+      <slot></slot>
+    </div>
+  </button>
 </template>
 
 <script>
-export default {}
+export default {
+  // props:['icon','iconPosition']
+  props:{
+    icon:{},
+    loading:{
+      type:Boolean,
+      default:false,
+    },
+    iconPosition: {
+      type: String,
+      default: 'left',
+      validator(value) {
+        return value === 'left' || value === 'right'
+      }
+    }
+  }
+}
 </script>
 
 <style lang="less">
+@keyframes spin {
+  0% {transform: rotate(0deg);}
+  100% { transform: rotate(360deg);}
+}
 .g-button {
   font-size: var(--font-size);
   height: var(--button-height);
@@ -14,6 +40,10 @@ export default {}
   border-radius: var(--border-radius);
   border: 1px solid var(--border-color);
   background: var(--button-bg);
+  display:inline-flex;
+  justify-content: center;
+  align-items: center;
+  vertical-align: middle;
   &:hover {
     border-color: var(--border-color-hover);
   }
@@ -22,6 +52,15 @@ export default {}
   }
   &:focus {
     outline: none;
+  }
+  > .icon{order:1; margin-right: .1em}
+  > .content{order:2;}
+  &.icon-right {
+    > .content {order:1;}
+    > .icon {order: 2; margin-right: 0; margin-left: .1em}
+  }
+  .loading{
+    animation: spin 2s infinite linear;
   }
 }
 </style>
