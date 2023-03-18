efa51bd2aec2fab8bfc74756b1c9351496608117
lijun <1094453726@qq.com>
2023-03-19 00:20:07 +0800

    更改package

--- src/app.js
+++ src/app.js
@@ -2,6 +2,10 @@ import Vue from 'vue'
 import Button from "./button.vue";
 import Icon from "./icon.vue";
 import ButtonGroup from './button-group.vue'
+import chai from 'chai'
+import spies from 'chai-spies'
+chai.use(spies)//让chai使用这个间谍监听函数
+const expect = chai.expect
 
 Vue.component('g-button',Button)
 Vue.component('g-icon',Icon)
@@ -12,3 +16,65 @@ new Vue({
     loading1: false,
   }
 })
+{
+  const Constructor = Vue.extend(Button)
+  const vm = new Constructor({
+    propsData: {
+      icon: 'settings'
+    }
+  })
+  vm.$mount()
+  let useElement = vm.$el.querySelector('use')
+  let href = useElement.getAttribute('xlink:href')
+  expect(href).to.eq('#i-settings')
+  vm.$el.remove()
+  vm.$destroy()
+}
+
+{
+  const Constructor = Vue.extend(Button)
+  const vm = new Constructor({
+    propsData: {
+      icon: 'settings',
+      loading: true
+    }
+  })
+  vm.$mount()
+  let useElement = vm.$el.querySelector('use')
+  let href = useElement.getAttribute('xlink:href')
+  expect(href).to.eq('#i-loading')
+  vm.$el.remove()
+  vm.$destroy()
+}
+{
+  const div = document.createElement('div')
+  document.body.appendChild(div)
+  const Constructor = Vue.extend(Button)
+  const vm = new Constructor({
+    propsData:{
+      icon:'settings'
+    }
+  })
+  vm.$mount(div)
+  let svg = vm.$el.querySelector('svg')
+  let {order} = window.getComputedStyle(svg)
+  expect(order).to.eq('1')
+  vm.$el.remove()
+  vm.$destroy()
+}
+
+{
+  const Constructor = Vue.extend(Button)
+  const vm = new Constructor({
+    propsData:{
+      icon:'settings',
+    }
+  })
+  vm.$mount()
+  let spy = chai.spy(function(){})
+  vm.$on('click',spy)
+  //希望这个函数被执行
+  let button = vm.$el
+  button.click()
+  expect(spy).to.have.been.called()//期待我们的间谍函数已经被调用了
+}
